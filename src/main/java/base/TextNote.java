bb2c0f5e0aef82bb5dc8021f3f1fe670af64254c
henrywong <henrywong20040406@gmail.com>
2024-10-31 21:11:44 +0800

    lab7

--- src/main/java/base/TextNote.java
+++ src/main/java/base/TextNote.java
@@ -2,6 +2,7 @@ package base;
 
 import javax.swing.Icon;
 import java.io.*;
+import java.util.HashMap;
 
 public class TextNote extends Note implements java.io.Serializable, Iconifiable {
 
@@ -83,5 +84,27 @@ public class TextNote extends Note implements java.io.Serializable, Iconifiable 
         } else if ('0' <= firstCharacter && firstCharacter <= '9') {
             content = new IconDigit(firstCharacter).base + content.substring(1);
         }
+    }
+
+    public Character unknownFunction(){
+        HashMap<Character,Integer> count = new HashMap<Character,Integer>();
+        String a = this.getTitle() + this.getContent();
+        int b = 0;
+        Character r = ' ';
+        for (int i = 0; i < a.length(); i++) {
+            Character c = a.charAt(i);
+            if (c <= 'Z' && c >= 'A' || c <= 'z' && c >= 'a') {
+                if (!count.containsKey(c)) {
+                    count.put(c, 1);
+                } else {
+                    count.put(c, count.get(c) + 1);
+                    if (count.get(c) > b) {
+                        b = count.get(c);
+                        r = c;
+                    }
+                }
+            }
+        }
+        return r;
     }
 }
---
+++ src/test/java/base/JUnitTest.java
@@ -0,0 +1,48 @@
+package base;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.HashSet;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import base.Note;
+import base.NoteBook;
+import base.TextNote;
+
+public class JUnitTest {
+
+	@Test
+	public void testSearchNote() {
+		NoteBook nb = new NoteBook();
+		nb.createTextNote("Note1", "Java", "comp3021");
+		nb.createTextNote("Note2", "Assignment", "due on 2023-10-13");
+		nb.createTextNote("Note3", "lab","need to attend weekly");
+		nb.createTextNote("Note4", "lab7","testing");
+		List<Note> notes = nb.searchNotes("java or DUE or testing");
+		System.out.println(notes.size());
+		assertEquals(3, notes.size(), "The size of the search results is not match");
+		HashSet<String> titles = new HashSet<>();
+		for (Note note : notes) {
+			titles.add(note.getTitle());
+		}
+		HashSet<String> expectedOutputs = new HashSet<>();
+		expectedOutputs.add("Java");
+		expectedOutputs.add("Assignment");
+		expectedOutputs.add("lab7");
+		assertEquals(expectedOutputs, titles, "The search results is not match");
+	}
+	
+	// To do
+	// Design the second test case which reveals the bug in function unknownFunction()
+	@Test
+	public void testUnknownFunction() {
+		NoteBook nb = new NoteBook();
+		nb.createTextNote("Note5", "Java", "comp3021");
+
+		Character actual = ((TextNote)nb.getFolders().get(0).getNotes().get(0)).unknownFunction();
+		Character expectedOutputs = 'a';
+		assertEquals(expectedOutputs, actual, "The search results is not match");
+	}
+}
